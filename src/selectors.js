3de9206ab9e66f0d77a16ae3fcded7c7b4ebade9
franetti <66683602+franetti@users.noreply.github.com>
2021-12-31 14:48:57 -0300

    Add files via upload

---
+++ src/selectors.js
@@ -0,0 +1,4 @@
+export const hamburger = document.querySelector('#hamburger');
+export const arrTargetsObserver = document.querySelectorAll('.observer');
+export const form = document.getElementById("my-form");
+export const formIndex = document.getElementById("my-form-index");
