071370e4bbc91d34f792d5e7037ab28353988b33
franetti <66683602+franetti@users.noreply.github.com>
2021-12-31 14:37:51 -0300

    Delete src directory

--- src/selectors.js
+++
@@ -1,4 +0,0 @@
-export const hamburger = document.querySelector('#hamburger');
-export const arrTargetsObserver = document.querySelectorAll('.observer');
-export const form = document.getElementById("my-form");
-export const formIndex = document.getElementById("my-form-index");
